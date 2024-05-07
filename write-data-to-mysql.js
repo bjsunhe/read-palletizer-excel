d0f311377307f39c6cd3aa9022bd1730d31e20fb
SUN He (BMG-3CN/PJ-CDT) <snh3sgh@bosch.com>
2024-05-07 22:03:23 +0800

    write

--- write-data-to-mysql.js
+++ write-data-to-mysql.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
-  password: 'Cool123456789',
+  password: 'Cool1234567890-',
   database: 'bmg'
 });
 
